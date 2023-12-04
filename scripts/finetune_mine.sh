0bd5449a4a612b58141bc2254dc8861a438a70e0
Amin <aminreza_sefid@yahoo.com>
2023-12-04 18:02:19 +0330

    some changes

--- scripts/finetune_mine.sh
+++ scripts/finetune_mine.sh
@@ -12,7 +12,7 @@ for dataset in $datasets; do
                   --batch_size=$batch_size --max_epoch=$epoch --dataset_name=$dataset \
                   --split_type=scaffold --data_path="./Raw_MoleculeNet/$dataset" \
                   --seed=$num \
-                  --cached_data_path="./Pre-Processed_TestSplit_MoleculeNet/$dataset/$mode/TrainValid/" --compound_encoder_config=model_configs/geognn_l8.json \
+                  --cached_data_path="./Pre-Processed_TestSplit_MoleculeNet/$dataset/$mode/" --compound_encoder_config=model_configs/geognn_l8.json \
                   --model_config=model_configs/down_mlp2.json --init_model="./pretrain_models-chemrl_gem/regr.pdparams" \
                   --model_dir="./output/finetune/$dataset/$mode/seed=$num" --encoder_lr=1e-3 --head_lr=4e-3 \
                   --dropout_rate=0.1 > "./$dataset-$mode-result$num.txt"
